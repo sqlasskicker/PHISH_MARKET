78ab47cb73b37f2823680a26d99c605e15d9e1ec
sqlasskicker <63858650+sqlasskicker@users.noreply.github.com>
2020-10-01 16:23:35 +0530

    Add

---
+++ phish_market.py
@@ -0,0 +1,234 @@
+import os
+import time
+os.system("clear")
+while True:
+    print("")  
+    print("\033[34m   ╔═╗╦ ╦╦╔═╗╦ ╦  ╔╦╗╔═╗╦═╗╦╔═╔═╗╔╦╗")
+    print("\033[34m   ╠═╝╠═╣║╚═╗╠═╣  ║║║╠═╣╠╦╝╠╩╗║╣  ║ ")
+    print("\033[34m   ╩  ╩ ╩╩╚═╝╩ ╩  ╩ ╩╩ ╩╩╚═╩ ╩╚═╝ ╩ ")
+    print("\033[34m   ==============××××===============")
+
+    os.system("echo")
+    time.sleep(0.5)
+    print("\033[31m [•] AUTHOR : SQL ASSKICKER")
+    os.system("echo")
+    print("\033[31m [•] GITHUB : https://github.com/sqlasskicker") 
+    os.system("echo")
+
+
+    print("\033[36m   ^ ^  +-+-+-+-+-+  ^ ^ ")
+    print("\033[36m   * *  |T|O|O|L|S|  * * ")
+    print("\033[36m    •   +-+-+-+-+-+   •  ")
+
+
+    os.system("echo")
+    print("")
+    print("\033[32m  [1]  Hidden Eye")
+    print ("")
+    print("\033[32m  [2]  ZPhisher")
+    print("")
+    print("\033[32m  [3]  NexPhisher")
+    print ("")
+    print("\033[32m  [4]  SocialFish")
+    print ("")
+    print("\033[32m  [5]  Weeman")
+    print("")
+    print("\033[32m  [6]  AdvPhishing")
+    print("")
+    print("\033[32m  [7]  Shark")
+    print("")
+    print("\033[32m  [8]  Shellphish")
+    print("")
+    print("\033[32m  [9]  BlackEye")
+    print("")
+    print("\033[32m  [10] Install All Tools")
+    print("")
+    print("\033[32m  [11] EXIT ")
+    print("")
+    x = int(input("\033[31m [•] Enter Your Choice :  "))
+   
+
+    if x == 11:
+        os.system("bash")
+    print("")
+  
+    if x >11:
+        time.sleep(0.5)
+        print("\033[32m  ERROR INVALID OPTION !!")
+        continue
+        break
+        print ("")
+    if x <1:
+        time.sleep(0.5)
+        print("\033[32m  ERROR INVALID OPTION !!")
+        continue
+        break
+
+        print ("")
+
+#hiddeneye
+    if x == 1:
+        print("\033[32m  Initilizing Install ....")
+        print ("")
+        os.system("git clone https://github.com/DarkSecDevelopers/HiddenEye")
+        print("")
+        time.sleep(0.5)
+        print("\033[31m  DONE")
+        print ("")
+        print("\033[31m [•] USAGE :\033[33m  python HiddenEye.py or ./HiddenEye.py ")
+        time.sleep(1)
+        continue
+        break
+
+        print("")
+
+#zphisher
+    if x == 2:
+        print("\033[32m  Initilizing Install ....")
+        print("")
+        os.system("git clone https://github.com/htr-tech/zphisher")
+
+        print("")
+        time.sleep(0.5)
+        print("\033[31m  DONE")
+        print ("")
+        print("\033[31m [•] USAGE :\033[33m   bash zphisher.sh")
+        time.sleep(1)
+        continue
+        break
+
+        print("")
+
+#Nexphisher
+    if x == 3:
+        print("\033[32m  Initilizing Install ....")
+        print("")
+        os.system("git clone https://github.com/htr-tech/nexphisher")
+        print("")
+        time.sleep(0.5)
+        print("\033[31m  DONE")
+        print ("")
+        print("\033[31m [•] USAGE :\033[33m    bash nexphisher")
+        time.sleep(1)
+        continue
+        break
+        print("")
+
+#Socialfish
+    if x == 4:
+        print("\033[32m  Initilizing Install ....")
+        print("")
+        os.system("git clone https://github.com/xHak9x/SocialPhish")
+        print("")
+        time.sleep(0.5)
+        print("\033[31m  DONE")
+        print ("")
+        print("\033[31m [•] USAGE :\033[33m    ./socialphish.sh")
+        time.sleep(1)
+        continue
+        break
+        print("")
+
+#weeman
+    if x == 5:
+        print("\033[32m  Initilizing Install ....")
+        print("")
+        os.system("git clone https://github.com/samyoyo/weeman")
+        print("")
+        time.sleep(0.5)
+        print("\033[31m  DONE")
+        time.sleep(1)
+        continue
+        break
+        print("")
+
+#Advphishing
+    if x == 6:
+        print("\033[32m  Initilizing Install ....")
+        print("")
+        os.system("git clone https://github.com/Ignitetch/AdvPhishing")
+        print("")
+        time.sleep(0.5)
+        print("\033[31m  DONE")
+        print ("")
+        print("\033[31m [•] USAGE :\033[33m    ./AdvPhishing.sh")
+        time.sleep(1)
+        continue
+        break
+        print("")
+
+#shark
+    if x == 7:
+        print("\033[32m  Initilizing Install ....")
+        print("")
+        os.system("git clone https://github.com/Ignitetch/shark")
+        print("")
+        time.sleep(0.5)
+        print("\033[31m  DONE")
+        print ("")
+        print("\033[31m [•] USAGE :\033[33m    ./setup and Then Type shark")
+        time.sleep(1)
+        continue
+        break
+        print("")
+
+#shellphish
+    if x == 8:
+        print("\033[32m  Initilizing Install ....")
+        print("")
+        os.system("git clone https://github.com/suljot/shellphish")
+        print("")
+        time.sleep(0.5)
+        print("\033[31m  DONE")
+        print ("")
+        print("\033[31m [•] USAGE :\033[33m    bash shellphish.sh")
+        time.sleep(1)
+        continue
+        break
+        print("")
+
+#blackeye
+    if x == 9:
+        print("\033[32m  Initilizing Install ....")
+        print("")
+        os.system("git clone https://github.com/8L4NK/blackeye")
+        print("")
+        time.sleep(0.5)
+        print("\033[31m  DONE")
+        print ("")
+        print("\033[31m [•] USAGE :\033[33m    bash blackeye.sh")
+        time.sleep(1)
+        continue
+        break
+        print("")
+#all
+    if x == 10:
+        print("\033[32m  Initilizing Install It Will Take Some Time  ....")
+        print("")
+        #hiddeneye
+        os.system("git clone https://github.com/DarkSecDevelopers/HiddenEye")
+        print ("")
+        #zphisher
+        os.system("git clone https://github.com/htr-tech/zphisher")
+        print ("")
+        #nexphisher
+        os.system("git clone https://github.com/htr-tech/nexphisher")
+        print ("")
+        #socialphish
+        os.system("git clone https://github.com/xHak9x/SocialPhish")
+        print ("")
+        #weeman
+        os.system("git clone https://github.com/samyoyo/weeman")
+        print ("")
+        #advphishing
+        os.system("git clone https://github.com/Ignitetch/AdvPhishing")
+        print ("")
+        #shark
+        os.system("git clone https://github.com/Ignitetch/shark")
+        print ("")
+        #shellphish
+        os.system("git clone https://github.com/suljot/shellphish")
+        print ("")
+        #blackeye
+        os.system("git clone https://github.com/8L4NK/blackeye")
+        print ("")
